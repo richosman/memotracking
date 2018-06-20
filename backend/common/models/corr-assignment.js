25e5bdb0112ece54e06c7ba1c6eb23c5a5a6c407
richosmanthus <richosmanthus@gmail.com>
2018-06-20 22:33:06 +0000

    email sending phase 1 completed

--- backend/common/models/corr-assignment.js
+++ backend/common/models/corr-assignment.js
@@ -2,4 +2,29 @@
 
 module.exports = function(Corrassignment) {
 
+  Corrassignment.observe('after save', function(ctx, next) {
+
+    console.log('ctx',ctx)
+
+    console.log('supports isNewInstance?', ctx.isNewInstance !== undefined);
+    next();
+  });
+
+  Corrassignment.sendEmail = function (cb) {
+    console.log('cb',cb)
+    Corrassignment.app.models.Email.send({
+
+      to: 'jmosesessel21@gmail.com',
+      from: 'jmosesessel21@gmail.com',
+      subject: 'test mail',
+      text: 'my text',
+      html: 'Testing mail settings'
+    },function (err, mail) {
+      console.log('email sent!', mail)
+      console.log('err', err)
+      return cb(null)
+    })
+  }
+
+  Corrassignment.remoteMethod('sendEmail', {returns: {type:'string'}})
 };
